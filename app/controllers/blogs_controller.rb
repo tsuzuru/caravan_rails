e9497577480aedb295d18eb93d42c88e22ff2f49
tsuzuru <h.tsuzuruki@gmail.com>
2021-03-09 08:54:53 +0000

    5-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def index
+    # 記事を全件取得
     @blogs = Blog.all
   end
 
@@ -14,22 +15,27 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blog_path(blog.id)
+    redirect_to blog_path(blog.id)  #この行を修正
   end
 
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
 
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-
 end
